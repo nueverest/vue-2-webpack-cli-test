e0d7fef91a25dfb93841101576f361590ec0fbdf
Chad Nelson <nu.everest@gmail.com>
2016-09-08 05:28:21 -0700

    Manual initial upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <VComponent></VComponent>
+    <v-component></v-component>
+    <vapp></vapp>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  import VComponent from './components/v-component.vue'
+
+  export default {
+    components () {
+      VComponent
+    },
+    data () {
+      return {
+        msg: 'Hello Vue 2.0!'
+      }
+    }
+  }
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
